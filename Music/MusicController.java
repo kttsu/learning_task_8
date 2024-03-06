23d693959fbb707e45727fbdea803058b413348d
Tsuchiya Kanm <150462533+kttsu@users.noreply.github.com>
2024-03-06 22:35:11 +0900

    Update MusicController.java
    ExceptionHandlerを別クラスに打ち出しました。

--- Music/MusicController.java
+++ Music/MusicController.java
@@ -3,7 +3,6 @@ package com.example.Music;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,18 +21,4 @@ public class MusicController {
     public Music getMusic(@PathVariable("id") int id) {
         return musicService.findMusic(id);
       }
-
-@ExceptionHandler(value = UserNotFoundException.class)
-    public ResponseEntity<Map<String, String>> handleUserNotFoundException(
-            UserNotFoundException e, HttpServletRequest request) {
-        Map<String, String> body = Map.of(
-                "timestamp", ZonedDateTime.now().toString(),
-                "status", String.valueOf(HttpStatus.NOT_FOUND.value()),
-                "error", HttpStatus.NOT_FOUND.getReasonPhrase(),
-                "message", e.getMessage(),
-                "path", request.getRequestURI());
-        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
-    }
-
-
 }
