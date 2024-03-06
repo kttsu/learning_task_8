b1f962145c618e518d90da07d2ebc5d09961cdca
Tsuchiya Kanm <150462533+kttsu@users.noreply.github.com>
2024-03-06 22:22:19 +0900

    Update MusicMapper.java
    不要なメッソドを削除しました。

--- Music/MusicMapper.java
+++ Music/MusicMapper.java
@@ -10,7 +10,4 @@ import java.util.Optional;
 public interface MusicMapper {
     @Select("SELECT * FROM music WHERE id = #{id}")
     Optional<Music> findById(int id);
-    @Select("SELECT * FROM music WHERE name LIKE CONCAT(#{prefix}, '%') AND name LIKE CONCAT('%',#{suffix}) AND name LIKE CONCAT('%',#{contains}, '%')")
-    List<Music> findByMusicStartingWith(String prefix, String suffix, String contains);
-
 }
